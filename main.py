fe7375151415d730cabb4a919874c01c0bb31159
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-06 22:36:06 +0000

    Completed the password-generator exercise code.

--- main.py
+++ main.py
@@ -13,5 +13,40 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 
+# password=""
+# for char in range(1,nr_letters+1): 
+ 
+#   password+=random.choice(letters) 
+
+ 
+
+# for char in range(1,nr_symbols+1):
+#   password+=random.choice(symbols)  
+# for char in range(1,nr_numbers+1):
+#   password+=random.choice(numbers) 
+
+# print(password)
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password_list=[]
+for char in range(1,nr_letters+1): 
+ 
+  password_list+=random.choice(letters) 
+
+ 
+
+for char in range(1,nr_symbols+1):
+  password_list+=random.choice(symbols)  
+for char in range(1,nr_numbers+1):
+  password_list+=random.choice(numbers) 
+
+print(password_list)
+random.shuffle(password_list) 
+print(password_list) 
+
+password="" 
+
+for char in password_list:
+  password+=char 
+
+print(f"Your password is : {password}")
