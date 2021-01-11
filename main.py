b013c056a7995206876edb4aa2dde5098e9b3fd3
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-11 23:07:34 +0000

    Further testing and created a blank list to add the correct word to it.

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ word_list=["ardvark","baboon","camel"]
 import random 
 chosen_word=random.choice(word_list) 
 print(chosen_word)
+display=[] 
+world_length=len(chosen_word)
+for _ in range(world_length):
+  display+="_"
+print(display)
+
 
 # To Do 2 Ask the to guess a letter and assign their answer to a variable called guess.Make the guess lowercase.
 guess=input("Guess a Letter: ").lower()
 
 
 # To Do 3 Check if the letter user guessed is one of the letters in the chosen word  
-for letter in chosen_word:
-  if letter==guess:
-    print("Right")
-  else: 
-      print("Wrong")
+for position in range(world_length):
+  letter=chosen_word[position] 
+  if letter==guess: 
+     display[position]=letter
+
+print(display)     
