31805749d0aae365137896a2061ebd670b4b7640
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-11 22:32:23 +0000

    Started a basic layout for hangman.

--- main.py
+++ main.py
@@ -1,52 +1,16 @@
-#Password Generator Project
-import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# Step 1 
+word_list=["ardvark","baboon","camel"]
+# To Do -1 Randomly choose a word from the word_list and assign it to a variable caslled chosen word 
+import random 
+chosen_word=random.choice(word_list)
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+# To Do 2 Ask the to guess a letter and assign their answer to a variable called guess.Make the guess lowercase.
+guess=input("Guess a Letter: ").lower()
 
 
-# password=""
-# for char in range(1,nr_letters+1): 
- 
-#   password+=random.choice(letters) 
-
- 
-
-# for char in range(1,nr_symbols+1):
-#   password+=random.choice(symbols)  
-# for char in range(1,nr_numbers+1):
-#   password+=random.choice(numbers) 
-
-# print(password)
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-password_list=[]
-for char in range(1,nr_letters+1): 
- 
-  password_list+=random.choice(letters) 
-
- 
-
-for char in range(1,nr_symbols+1):
-  password_list+=random.choice(symbols)  
-for char in range(1,nr_numbers+1):
-  password_list+=random.choice(numbers) 
-
-print(password_list)
-random.shuffle(password_list) 
-print(password_list) 
-
-password="" 
-
-for char in password_list:
-  password+=char 
-
-print(f"Your password is : {password}")
+# To Do 3 Check if the letter user guessed is one of the letters in the chosen word  
+for letter in chosen_word:
+  if letter==guess:
+    print("Right")
+  else: 
+      print("Wrong")
