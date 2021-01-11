e4cd544c3893aff62cf7df739c1ed79ae9e2ad7e
sumityadav1509 <sumityadav2005@gmail.com>
2021-01-11 22:35:56 +0000

    Used print function to test .

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 word_list=["ardvark","baboon","camel"]
 # To Do -1 Randomly choose a word from the word_list and assign it to a variable caslled chosen word 
 import random 
-chosen_word=random.choice(word_list)
+chosen_word=random.choice(word_list) 
+print(chosen_word)
 
 # To Do 2 Ask the to guess a letter and assign their answer to a variable called guess.Make the guess lowercase.
 guess=input("Guess a Letter: ").lower()
